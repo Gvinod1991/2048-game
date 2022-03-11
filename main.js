0708696847793ca49ca864a4a0074d9574222f2f
Godti Vinod <venkat.godti3@gmail.com>
2022-03-11 22:59:20 +0530

    Tile class  implementation and game board initialization

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 const gameBoard = document.querySelector('#game-board');
 
 const game = new Grid(gameBoard);
+game.randomEmptyCell().tile = new Tile(gameBoard);
+game.randomEmptyCell().tile = new Tile(gameBoard);
